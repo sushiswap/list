4f710ef491dd9a95eca48f268dbc6348475b3115
Matthew Lilley <hello@matthewlilley.com>
2022-01-19 01:01:02 +0000

    fix: build and avax list

--- packages/limit-order-pair-list/internal/build.js
+++ packages/limit-order-pair-list/internal/build.js
@@ -1,6 +1,6 @@
 const { version } = require("../package.json");
 
-const { ChainId } = require("@sushiswap/sdk");
+const { ChainId } = require("@sushiswap/core-sdk");
 
 const fs = require("fs");
 
@@ -10,6 +10,7 @@ const DEFAULT_TOKEN_LIST = require("@sushiswap/default-token-list");
 
 const SUPPORTED_CHAINS = {
   [ChainId.MATIC]: "matic",
+  [ChainId.AVALANCHE]: "avalanche",
 };
 
 function getPairs() {
@@ -38,12 +39,12 @@ function getPairs() {
         return token;
       })
     );
-
-    return allPairs;
   }
+
+  return allPairs;
 }
 
-module.exports = function build() {
+module.exports = function () {
   const parsed = version.split(".");
   return {
     name: "SushiSwap Pair Menu",
